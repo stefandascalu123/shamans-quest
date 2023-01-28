83bd7c8f8a29a2b8f375bdda8bc65c026f0303bb
stefandascalu123 <64279272+stefandascalu123@users.noreply.github.com>
2023-01-28 22:44:50 +0200

    Update default-2021.dwlt

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float speed;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+}
