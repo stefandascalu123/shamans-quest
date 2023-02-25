921d8d609ed02abd450f41606e6b4f81148d0ec1
larisa-pavel <diana.larisa.pavel@gmail.com>
2023-02-25 04:00:24 +0200

    broken background
    detalii in scriptul din background

---
+++ Assets/Scripts/ShamanScripts/BackgroundParalex.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParalex : MonoBehaviour
+{
+    private float length, startPosition;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPosition + dist, transform.position.y, transform.position.z);
+        //ce e comentat nu merge for some reason
+        //codul de mai jos ar fi trebuit sa fie pentru infinite loop axa x, dar nu-i place
+        //motiv: nu stiu de ce se face mirror la imagine cand caracterul ia stanga dreapta (nu trebuia sa faca asa)
+        //solutie: cine vede asta si rezolva pana imi dau eu seama il pup la interzis uwu
+
+
+        //if(temp> startPosition + length)startPosition += length;
+        //else if (temp < startPosition + length)startPosition -= length;
+    }
+}
